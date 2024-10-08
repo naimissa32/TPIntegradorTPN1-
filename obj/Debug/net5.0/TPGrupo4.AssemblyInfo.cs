ebb226b41036d965d8daab74c7a5cd106729decf
naimi <naimissa32@hotmail.com>
2024-10-07 21:24:30 -0300

    se agrega paquete

--- obj/Debug/net5.0/TPGrupo4.AssemblyInfo.cs
+++ obj/Debug/net5.0/TPGrupo4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TPGrupo4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+692f66994ed4e00b102ea40188737d3c32bccbf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TPGrupo4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TPGrupo4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
